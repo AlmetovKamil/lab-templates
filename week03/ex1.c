4e1d622a070b7b2c4f06a11ae97c60ac4b87b29a
firas-jolha <firasjolha@gmail.com>
2022-09-15 12:25:22 +0300

    add skeleton code

---
+++ week03/ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int foo(int age){
+	int result;
+	
+	//<WRITE YOUR CODE HERE>
+	
+	return result;
+}
+
+int main(){
+
+	//<WRITE YOUR CODE HERE>
+
+	return EXIT_SUCCESS;
+}
---
+++ week03/ex2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+<WRITE YOUR CODE HERE>
+
+typedef struct{
+
+	//<WRITE YOUR CODE HERE>
+	
+} Point;
+
+float distance(Point A, Point B){
+	float result;
+	
+	//<WRITE YOUR CODE HERE>
+	
+	return result;
+}
+
+float area(Point A, Point B, Point C){
+	float result;
+	
+	//<WRITE YOUR CODE HERE>
+	
+	return result;
+}
+
+int main(void){
+
+	//<WRITE YOUR CODE HERE>
+		
+
+	float f = distance(A, B);
+	printf("A -- B distance is %f\n", f);
+
+
+	float a = area(A, B, C);
+	printf("Area of triangle ABC is %f\n", a);
+
+	return EXIT_SUCCESS;
+}
